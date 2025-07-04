0ce0f3f7e0e788dd9edd3ea83e246970b8aba70f
Tanisha-773 <rathodtanisha15@gmail.com>
2025-07-04 11:35:12 +0530

    Done

---
+++ t1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class t1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
